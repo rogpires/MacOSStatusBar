69fe48746b18f0f6a63818e0ba73629671e282f0
Rogerio Cesar Gomes Pires <rogeriocpires@hotmail.com>
2022-09-05 23:33:52 -0300

    Initial Commit

---
+++ MacOSStatusBar/MacOSStatusBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSStatusBarApp.swift
+//  MacOSStatusBar
+//
+//  Created by Rogerio Pires on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MacOSStatusBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
